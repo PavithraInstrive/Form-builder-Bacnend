238800e50ca7ebe1086c2f15700003ff36c4458a
PavithraInstrive <pavithra@instrive.in>
2025-07-24 16:50:37 +0530

    service account changed to env

--- index.js
+++ index.js
@@ -5,13 +5,35 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./form-builder-a6dce-firebase-adminsdk-fbsvc-ff44dd2a6c.json');
+
+
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
+
+// const serviceAccount = require('./form-builder-a6dce-firebase-adminsdk-fbsvc-ff44dd2a6c.json');
 
 
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json()); 
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
